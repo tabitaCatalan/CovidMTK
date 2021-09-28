5ece5b075e8ecbb82e024711259d6ceedea96e66
Tabita <tabicm.nhg@gmail.com>
2021-09-28 15:49:12 -0300

    feat(plotting): scientific notation, common x axis, subindex labels

---
+++ plotting.jl
@@ -0,0 +1,70 @@
+#=
+Aux functions for plotting 
+=#
+
+#===========================
+Symbolics variables to Strings 
+===========================# 
+subindexs = Dict(
+    "0" => "₀",
+    "1" => "₁",
+    "2" => "₂",
+    "3" => "₃",
+    "4" => "₄",
+    "5" => "₅",
+    "6" => "₆",
+    "7" => "₇",
+    "8" => "₈",
+    "9" => "₉",
+)
+
+
+"""
+Transform a string of a number to a string of the number using subindexs.
+# Example 
+```julia
+julia> to_subindex("1729")
+"₁₇₂₉"
+```
+"""
+to_subindex(strnum) = join([subindexs[digit] for digit in split(strnum, "")])
+
+"""
+Return a string of a symbolic ModelingToolkit array variable using subindexs.
+- `var`: symbolic array ModelingToolkit variable 
+# Example 
+```julia
+@variables t S[1,2](t)
+string(S[1]) #"S[1](t)"
+to_string(S, 1) #"S₁(t)"
+```
+"""
+function to_string(var, index)
+    longstrvar = string(var[index])
+    strvar = longstrvar[1:findfirst('[', longstrvar)-1]
+    strvar * to_subindex(string(index)) * "(t)"
+end  
+
+
+#===========================
+Plot with scientific notation 
+===========================# 
+calculate_exponent(ymin, ymax) = round(Int, log10(ymax - ymin)) + 1  
+calculate_exponent(ymin, ymax) = floor(Int, log10(ymax)) 
+minmax(sol, var) = (minimum([sol[var[1]] sol[var[2]]]), maximum([sol[var[1]] sol[var[2]]]))
+
+function plot_scnotation!(a_plot, sol, var, subplot, scatter = false)
+    a = calculate_exponent(minmax(sol,var)...)
+    for index in 1:length(var)
+        label = to_string(var, index)
+        if scatter
+            scatter!(a_plot, sol.t, sol[var[index]]/10^a, label = label, ylabel = "people", subplot = subplot, xticks = :none, ms = 1, msw = 0)
+        else 
+            plot!(a_plot, sol.t, sol[var[index]]/10^a, label = label, ylabel = "people", subplot = subplot, xticks = :none)
+        end 
+    end 
+    #annotate!(a_plot, (-0.085, 0.98), text("x10^$a", :left, 9)) # esto funcionaba con 1 plot 
+    annotate!(a_plot, (-0.08, 1.1), text("x10^$a", :left, 8), subplot = subplot)    
+end 
+
+
